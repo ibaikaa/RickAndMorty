8d30b07b0b36faed8c31b9ba84b52fef1d10e933
ibaikaa <ibaikaaaa@gmail.com>
2023-06-07 13:29:29 +0600

    Updated code in NetworkLayer

--- RickAndMorty/Services/Network/NetworkLayer.swift
+++ RickAndMorty/Services/Network/NetworkLayer.swift
@@ -29,38 +29,39 @@ final class NetworkLayer {
             default: return nil
         }
     }
+    
+    private func getDataForRequest<T: Decodable>(
+        _ request: URLRequest
+    ) async throws -> T {
+        let (data, response) = try await session.data(for: request)
+        if let error = validateError(for: response) { throw error }
+        return try decode(data: data)
+    }
 
     public func getCharacters(page: Int) async throws -> CharacterGroup {
-        guard let url = ApiRouter.getCharacters(page: page).url else { throw ApiError.invalidURL }
+        guard let url = ApiRouter
+            .getCharacters(page: page)
+            .url
+        else { throw ApiError.invalidURL }
         
-        let request = URLRequest(url: url)
-        let (data, response) = try await session.data(for: request)
-        
-        if let error = validateError(for: response) { throw error }
-        
-        return try decode(data: data)
+        return try await getDataForRequest(URLRequest(url: url))
     }
     
     public func getEpisodes(page: Int) async throws -> EpisodesGroup {
-        guard let url = ApiRouter.getEpisodes(page: page).url else { throw ApiError.invalidURL }
+        guard let url = ApiRouter
+            .getEpisodes(page: page)
+            .url
+        else { throw ApiError.invalidURL }
         
-        let request = URLRequest(url: url)
-        let (data, response) = try await session.data(for: request)
-        
-        if let error = validateError(for: response) { throw error }
-        
-        return try decode(data: data)
+        return try await getDataForRequest(URLRequest(url: url))
     }
     
     public func getLocations(page: Int) async throws -> LocationsGroup {
-        guard let url = ApiRouter.getLocations(page: page).url else { throw ApiError.invalidURL }
+        guard let url = ApiRouter.getLocations(page: page).url else {
+            throw ApiError.invalidURL
+        }
         
-        let request = URLRequest(url: url)
-        let (data, response) = try await session.data(for: request)
-        
-        if let error = validateError(for: response) { throw error }
-        
-        return try decode(data: data)
+        return try await getDataForRequest(URLRequest(url: url))
     }
     
 }
